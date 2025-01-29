26126b9ac8d6a422489a1a5277e01cb7a08ab726
Marcin Sieradzki <mdsieradzki@gmail.com>
2025-01-29 01:37:10 +0100

    Initial Commit

---
+++ src/main/java/com/example/emiiweb/EmiiWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.emiiweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmiiWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmiiWebApplication.class, args);
+    }
+
+}
